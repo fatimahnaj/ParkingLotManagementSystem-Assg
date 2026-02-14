2ca767766ae1b0bbe618c34c5ab64caea8f6eb40
Fatimah Najihah <fatimahnajihah05@gmail.com>
2026-02-14 15:15:14 +0800

    added trial ui

---
+++ ui/Dashboard.java
@@ -0,0 +1,30 @@
+package ui;
+import java.awt.*;
+import javax.swing.*;
+
+class Dashboard extends JPanel {
+
+    public Dashboard(MainFrame frame) {
+
+        setLayout(new BorderLayout());
+
+        JPanel centerPanel = new JPanel(new BorderLayout());
+
+        JLabel label = new JLabel("sini screen dashboard", SwingConstants.CENTER);
+        label.setFont(new Font("Arial", Font.BOLD, 24));
+        JButton registerBtn = new JButton("Register");
+        registerBtn.addActionListener(e -> frame.showScreen("SCREEN2"));
+        centerPanel.add(label, BorderLayout.CENTER);
+        centerPanel.add(registerBtn, BorderLayout.SOUTH);
+
+
+        JPanel bottomPanel = new JPanel(new BorderLayout());
+        JButton adminBtn = new JButton("admin");
+        adminBtn.addActionListener(e -> frame.showScreen("SCREEN3"));
+        bottomPanel.add(adminBtn, BorderLayout.EAST);
+
+        add(centerPanel, BorderLayout.CENTER);
+        add(bottomPanel, BorderLayout.SOUTH);
+    }
+}
+
